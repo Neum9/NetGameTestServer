0c6c7a88ab4354dd34bf938f11809d23bb948286
Neum <1975584042@qq.com>
2020-05-03 14:28:30 +0800

    add test timer and serialization

---
+++ TestSerialization/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSerialization {
+
+    [Serializable]
+    class Player {
+        public int coin = 0;
+        public int money = 0;
+        public string name = "";
+    }
+
+
+    class Program {
+        static void Main(string[] args) {
+            funcDeSerializate();
+            Console.Read();
+        }
+
+        static void funcSerializate() {
+            Player player = new Player();
+            player.coin = 1;
+            player.money = 10;
+            player.name = "Cjc";
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("data.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, player);
+            stream.Close();
+        }
+
+        static void funcDeSerializate() {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("data.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+            Player player = (Player)formatter.Deserialize(stream);
+            stream.Close();
+            Console.WriteLine("coin {0}", player.coin);
+            Console.WriteLine("money {0}", player.money);
+            Console.WriteLine("name {0}", player.name);
+
+        }
+    }
+}
---
+++ TestTimer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace TestTimer {
+    class Program {
+        static void Main(string[] args) {
+            Timer timer = new Timer();
+            timer.AutoReset = true;
+            timer.Interval = 1000;
+            timer.Elapsed += new ElapsedEventHandler(Tick);
+            timer.Start();
+
+            Console.Read();
+        }
+
+        public static void Tick(object sender, System.Timers.ElapsedEventArgs e) {
+            Console.WriteLine("每秒执行一次!");
+        }
+    }
+}
