026d8a220595bbfc9712a8eda7ec17f90cfd5f6e
Neum <1975584042@qq.com>
2020-05-31 23:36:26 +0800

    add some class

--- NetGameServer/Program.cs
+++ NetGameServer/Program.cs
@@ -6,36 +6,42 @@ using System.Threading.Tasks;
 
 namespace NetGameServer {
     class Program {
-        static void Main(string[] args) {
-            DataMgr dataMgr = new DataMgr();
+        //static void Main(string[] args) {
+        //    DataMgr dataMgr = new DataMgr();
 
-            //register
-            bool ret = dataMgr.Register("Cjc", "123");
-            if (ret) {
-                Console.WriteLine("Register Done!");
-            } else {
-                Console.WriteLine("Error in registering!");
-            }
-            //create player
-            ret = dataMgr.CreatePlayer("cjc");
-            if (ret) {
-                Console.WriteLine("Create player Done!");
-            } else {
-                Console.WriteLine("Error in creating player!");
-            }
-            //get player Data
-            PlayerData pd = dataMgr.GetPlayerData("cjc");
-            //change player Data
-            pd.score += 10;
-            //save data
-            Player p = new Player();
-            p.id = "cjc";
-            p.data = pd;
-            dataMgr.SavePlayer(p);
-            //reload
-            pd = dataMgr.GetPlayerData("cjc");
-            Console.WriteLine("score " + pd.score);
-            Console.Read();
+        //    //register
+        //    bool ret = dataMgr.Register("Cjc", "123");
+        //    if (ret) {
+        //        Console.WriteLine("Register Done!");
+        //    } else {
+        //        Console.WriteLine("Error in registering!");
+        //    }
+        //    //create player
+        //    ret = dataMgr.CreatePlayer("cjc");
+        //    if (ret) {
+        //        Console.WriteLine("Create player Done!");
+        //    } else {
+        //        Console.WriteLine("Error in creating player!");
+        //    }
+        //    //get player Data
+        //    PlayerData pd = dataMgr.GetPlayerData("cjc");
+        //    //change player Data
+        //    pd.score += 10;
+        //    //save data
+        //    Player p = new Player();
+        //    p.id = "cjc";
+        //    p.data = pd;
+        //    dataMgr.SavePlayer(p);
+        //    //reload
+        //    pd = dataMgr.GetPlayerData("cjc");
+        //    Console.WriteLine("score " + pd.score);
+        //    Console.Read();
+        //}
+
+        static void Main(string[] args) {
+            ServNet servNet = new ServNet();
+            servNet.Start("127.0.0.1", 1234);
+            Console.ReadLine();
         }
     }
 }
---
+++ NetGameServer/core/Conn.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetGameServer {
+    class Conn {
+        //常量
+        public const int BUFFER_SIZE = 1024;
+        //Socket
+        public Socket socket;
+        public bool isUse = false;
+        //Buff
+        public byte[] readBuff = new byte[BUFFER_SIZE];
+        public int buffCount = 0;
+        //粘包分包
+        public byte[] lenByte = new byte[sizeof(UInt32)];
+        public Int32 msgLength = 0;
+        //心跳时间
+        public long lastTickTime = long.MinValue;
+        //对应的Player
+        public Player player;
+
+        //构造函数
+        public Conn() {
+            readBuff = new byte[BUFFER_SIZE];
+        }
+        //初始化
+        public void Init(Socket socket) {
+            this.socket = socket;
+            isUse = true;
+            buffCount = 0;
+            //心跳处理,稍后实现GetTimeStamp方法
+            lastTickTime = Sys.GetTimeStamp();
+        }
+        // 剩余的Buff
+        public int BuffRemain() {
+            return BUFFER_SIZE - buffCount;
+        }
+        //获取客户端地址
+        public string GetAddress() {
+            if (!isUse) {
+                return "无法获取地址";
+            }
+            return socket.RemoteEndPoint.ToString();
+        }
+        //关闭
+        public void Close() {
+            if (!isUse) {
+                return;
+            }
+            if (player != null) {
+                //TODOCjc 玩家退出处理
+                //player.Logout();
+                return;
+            }
+            Console.WriteLine("断开连接 " + GetAddress());
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+            isUse = false;
+        }
+
+        //TODOCjc 发送协议
+
+    }
+}
